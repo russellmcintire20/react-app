b229969add3cc585c1099ee623e66be140887e7e
Russell McIntire <russell.mcintire20@gmail.com>
2023-03-20 22:55:23 -0500

    adding li

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,18 @@
 function ListGroup() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0) return <p>No item found</p>;
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
