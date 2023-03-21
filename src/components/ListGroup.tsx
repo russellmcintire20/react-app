6f6d0f8b7160812d502069110fc3802d2d05c556
Russell McIntire <russell.mcintire20@gmail.com>
2023-03-20 23:02:36 -0500

    adding conditional logic

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,11 +2,10 @@ function ListGroup() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length === 0) return <p>No item found</p>;
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
